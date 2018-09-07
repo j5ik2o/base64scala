e0ca117482d32850e77cd50e16130410131864ab
Junichi Kato <j5ik2o@gmail.com>
2018-09-07 10:27:19 +0900

    reformat

--- library/src/test/scala/com/github/j5ik2o/base64scala/Base64StringSpec.scala
+++ library/src/test/scala/com/github/j5ik2o/base64scala/Base64StringSpec.scala
@@ -1,11 +1,11 @@
 package com.github.j5ik2o.base64scala
 
-import org.scalatest.{FreeSpec, Matchers}
+import org.scalatest.{ FreeSpec, Matchers }
 
 class Base64StringSpec extends FreeSpec with Matchers {
   "Base64String" - {
     "encode" in {
-      val string = "ABC"
+      val string       = "ABC"
       val base64String = Base64StringFactory().encode(string).right.get
       base64String.asString shouldBe "QUJD"
       base64String.decode.right.get shouldBe string.getBytes(base64String.charset)
